0bfcba7cce2ff9d64fcf4c80a425aeed19304718
moosbusch <gkappei@web.de>
2013-08-18 12:10:49 +0200

    License headers added

--- src/noNamespace/AltTermDocument.java
+++ src/noNamespace/AltTermDocument.java
@@ -1,10 +1,17 @@
-/*
- * An XML document type.
- * Localname: altTerm
- * Namespace: 
- * Java type: noNamespace.AltTermDocument
+/* 
+ * Copyright 2013 Gunnar Kappei.
  *
- * Automatically generated - do not modify.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package noNamespace;
 
